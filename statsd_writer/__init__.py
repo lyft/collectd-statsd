8138dd9a648a91fe58675f50c2e8eb73c0e64d93
Mathias Gug <mathias@lyft.com>
2015-06-29 17:55:53 -0700

    Fix style.

--- statsd_writer/__init__.py
+++ statsd_writer/__init__.py
@@ -22,11 +22,7 @@ A collectd plugin to write metrics to statsd.
 ######################
 from __future__ import (absolute_import, division,
                         print_function, unicode_literals)
-from builtins import (
-         bytes, dict, int, list, object, range, str,
-         ascii, chr, hex, input, next, oct, open,
-         pow, round, super,
-         filter, map, zip)
+from builtins import int
 
 #########################
 # Third Party Libraries #
